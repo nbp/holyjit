fb7a23d523e9ee091e79235957e500f297480ed5
Nicolas B. Pierron <nicolas.b.pierron@gmail.com>
2018-03-30 08:46:50 +0000

    Use the new rust-toolchain as the only source of truth for pulling rustc.

--- release.nix
+++ release.nix
@@ -3,13 +3,13 @@
 }:
 
 let
-  rustChannel = { channel = "nightly"; date = "2018-02-19"; };
+  rustChannel = { rustToolchain = ./rust-toolchain; };
   hj_overlay = self: super: {
     hj_rust = (super.rustChannelOf rustChannel).rust.override {
       extensions = [ "rust-src" ];
     };
     holyjit = super.callPackage ./default.nix {
-      rust =self.hj_rust;
+      rust = self.hj_rust;
     };
   };
   pkgs = import nixpkgs { overlays = [ (import rust_overlay) hj_overlay ];  };
